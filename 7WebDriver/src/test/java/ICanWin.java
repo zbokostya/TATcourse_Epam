dd3aec8276db3520e4433b17d0c70226fd6e8bbe
zbokostya <kostya.zboichik@gmail.com>
2020-12-04 18:20:10 +0300

    add Lab 7 WebDriver

---
+++ 7WebDriver/src/test/java/ICanWin.java
@@ -0,0 +1,57 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class ICanWin {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://pastebin.com");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testCreatePaste() {
+        WebElement pasteExpiration = waitForElementUntilAppearLocatedById(driver, By.xpath("//*[@id='w0']/div[5]/div[1]/div[2]/div/span/span[1]/span"));
+        pasteExpiration.click();
+
+        WebElement selectExpiration = waitForElementUntilAppearLocatedById(driver, By.xpath("//*[contains(@id,'select2-postform-expiration-result-') and contains(@id,'-10M')]"));
+        selectExpiration.click();
+
+        WebElement input = waitForElementLocatedById(driver, "postform-text");
+        input.sendKeys("Hello from WebDriver");
+
+        WebElement title = waitForElementLocatedById(driver, "postform-name");
+        title.sendKeys("helloweb");
+
+        WebElement createPaste = waitForElementUntilAppearLocatedById(driver, By.xpath("//div[@class='form-group form-btn-container']/button[@class='btn -big']"));
+        createPaste.click();
+    }
+
+    private static WebElement waitForElementLocatedBy(WebDriver webDriver, By by) {
+        return new WebDriverWait(webDriver, 10).until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private static WebElement waitForElementLocatedById(WebDriver webDriver, String id) {
+        return new WebDriverWait(webDriver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id(id)));
+    }
+
+    private static WebElement waitForElementUntilAppearLocatedById(WebDriver webDriver, By by) {
+        return new WebDriverWait(webDriver, 20).until(ExpectedConditions.elementToBeClickable(by));
+    }
+}
---
+++ 7WebDriver/src/test/java/TestAddToCart.java
@@ -0,0 +1,55 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class TestAddToCart {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.get("https://e-zoo.by/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testCreatePaste() throws InterruptedException {
+        if (driver.findElements(By.xpath("//div[@id='your_city-window']")).size() != 0) {
+            WebElement cityButton = waitForElementLocatedBy(driver, By.xpath("//div[@id='your_city-window']/form[@class='js-validation-form tex-center center']/button[@class='btn'][1]"));
+            cityButton.click();
+        }
+
+        WebElement searchInput = waitForElementLocatedBy(driver, By.xpath("//input[@class='search-field__input js-search-autocomplete']"));
+        searchInput.sendKeys("Brit care");
+
+        WebElement searchButton = waitForElementLocatedBy(driver, By.xpath("//button[@class='search-field__btn']"));
+        searchButton.click();
+
+        WebElement addToBucket = waitForElementLocatedBy(driver, By.xpath("//div[@class='catalog__item js-catalog-item'][2]/div[@class='cart js-cart']/div[@class='cart__inner']/div[@class='product__variant-table product__variant-table-small']/form[@class='product__variant js-cart-basket-submit']/div[@class='product__variant-cell product__variant-submit']/button[@class='btn btn_small basket-btn product__basket-btn js-cart-basket-btn ']"));
+        addToBucket.click();
+
+        WebElement goToBucket = waitForElementLocatedBy(driver, By.className("basket-field__icon"));
+        goToBucket.click();
+    }
+
+    private static WebElement waitForElementLocatedBy(WebDriver webDriver, By by) {
+        return new WebDriverWait(webDriver, 10).until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private static WebElement waitForElementLocatedById(WebDriver webDriver, String id) {
+        return new WebDriverWait(webDriver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id(id)));
+    }
+
+}
