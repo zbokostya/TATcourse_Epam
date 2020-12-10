c0915e33bbeaefcfb6e6848c89494d81405b8c64
zbokostya <kostya.zboichik@gmail.com>
2020-12-11 00:58:45 +0300

    fix test pastebin

--- 7WebDriver/src/test/java/TestICanWin.java
+++ 7WebDriver/src/test/java/TestICanWin.java
@@ -4,26 +4,46 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestICanWin {
     WebDriver driver;
 
     @Before
-    public void setUp() {
-        driver = new ChromeDriver();
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("browserVersion", "87.0");
+        Map<String, Object> cnt = new HashMap<>();
+        cnt.put("enableVNC", true);
+        cnt.put("enableVideo", false);
+        capabilities.setCapability("selenoid:options", cnt);
+        driver = new RemoteWebDriver(
+                URI.create("http://51.15.53.117:8080/wd/hub").toURL(),
+                capabilities
+        );
         driver.get("https://pastebin.com");
     }
 
     @After
     public void tearDown() {
-        driver.quit();
+//        driver.quit();
     }
 
     @Test
     public void testCreatePaste() {
+        if (driver.findElements(By.id("accept-choices")).size() != 0) {
+            WebElement acceptCookies = driver.findElement(By.id("accept-choices"));
+            acceptCookies.click();
+        }
         WebElement pasteExpiration = waitForElementUntilAppearLocatedById(driver, By.xpath("//*[@id='w0']/div[5]/div[1]/div[2]/div/span/span[1]/span"));
         pasteExpiration.click();
 
